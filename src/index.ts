2c87d8fe35dd7bf9220f5010f3b6c795acdfa411
anton-glebov <anton.glebov@useinsider.com>
2024-10-22 00:34:32 +0300

    feat: refactor worker ports

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,47 @@
+import cluster from 'node:cluster';
 import * as http from 'node:http';
 import 'dotenv/config'
 import { userRequestListener } from './routes';
 import { startCluster } from './cluster';
 
-const { PORT = 3000 } = process.env;
+const { HOST = 'localhost', PORT = 3000 } = process.env;
 
-const server = http.createServer(userRequestListener);
+let server: http.Server;
 
-function startServer(): void {
-  server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT} & PID: ${process.pid}. API on http://localhost:${PORT}`);
+function startServer(port: number): void {
+  server = http.createServer(userRequestListener);
+  server.listen(port, () => {
+    console.log(`Server running on port ${port} & PID: ${process.pid}. API on http://${HOST}:${port}/api/users/`);
   });
+
+  function shutdown() {
+    server.close((err) => {
+      if (err) {
+        console.error('Error while closing the server:', err);
+        process.exit(1);
+      }
+      console.log('Server closed successfully');
+
+      if (cluster.workers) {
+        for (const id in cluster.workers) {
+          cluster.workers[id]?.kill();
+        }
+      }
+
+      process.exit(0);
+    });
+  }
+
+  process.on('SIGINT', shutdown);
+  process.on('SIGTSTP', shutdown);
+  process.on('SIGTERM', shutdown);
 }
 
-function shutdown() {
-  server.close((err) => {
-    if (err) {
-      console.error('Error while closing the server:', err);
-      process.exit(1);
-    }
-    console.log('Server closed successfully');
-    process.exit(0);
-  });
-}
+startCluster(() => {
+  const workerPort = Number(PORT) + (cluster.worker?.id || 0);
+  startServer(workerPort);
+});
 
-process.on('SIGINT', shutdown);
-process.on('SIGTSTP', shutdown);
-process.on('SIGTERM', shutdown);
-startCluster(startServer);
+export const getServer = () => server;
 
 export { server, startServer };
